dda498849cf63d0c5269d9a7620715ba3e660b0a
SeungwanHong <swhgog@gmail.com>
2017-04-14 15:51:25 +0900

    20170414 commit

---
+++ web_20170414/WebContent/js/js01.js
@@ -0,0 +1,12 @@
+
+//자바스크립트 함수
+//function 함수이름(매개변수){ 구현 내용 }
+function foo(a){
+	alert(a);
+}
+
+//리턴이 있는 함수라도 함수에 리턴형태를 적어줄 필요가 없다.
+// 매개변수도 적어줄 필요가 없다.
+function goo(name){
+	return name + " Welcome"
+}
